a2677c3e63e3e37a412731393d4734d9f95f6334
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-03-05 00:04:45 +0000

    Temporarily disabling that

--- utils/r2.py
+++ utils/r2.py
@@ -77,7 +77,7 @@ async def upload_file_to_r2(
             content_type = "image/jpeg"
         elif filename.lower().endswith(("mp4", "avi", "mov", "webm")):
             content_type = "video/mp4"
-            file_bytes = await CompressVideo(file_bytes)
+            # file_bytes = await CompressVideo(file_bytes)
         elif filename.lower().endswith(("mp3", "wav", "ogg")):
             content_type = "audio/mpeg"
         else:
