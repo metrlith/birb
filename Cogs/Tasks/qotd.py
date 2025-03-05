e285eb3a1071873eec4709d80576b8e722c56fbf
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-03-05 18:29:56 +0000

    Make qotd faster

--- Cogs/Tasks/qotd.py
+++ Cogs/Tasks/qotd.py
@@ -15,74 +15,53 @@ MONGO_URL = os.getenv("MONGO_URL")
 environment = os.getenv("ENVIRONMENT")
 guildid = os.getenv("CUSTOM_GUILD")
 
+import asyncio
+
+
 class qotd(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
-    
-    
+        self.semaphore = asyncio.Semaphore(10)
+
     async def fetch_question(self, used_questions, server: discord.Guild):
-        questionresult = await self.client.db['Question Database'].find({}).to_list(length=None)
+        questionresult = (
+            await self.client.db["Question Database"].find({}).to_list(length=None)
+        )
         Unusued = [q for q in questionresult if q["question"] not in used_questions]
 
         if not Unusued:
-            await self.client.db['qotd'].update_one(
+            await self.client.db["qotd"].update_one(
                 {"guild_id": server.id}, {"$set": {"messages": []}}
             )
             return random.choice(questionresult).get("question")
         del questionresult
         return random.choice(Unusued).get("question")
-    
-    @tasks.loop(minutes=15, reconnect=True)
-    async def sendqotd(self) -> None:
-        print("[👀] Checking QOTD")
-        result = None
-        if bool(environment == "custom"):
 
-            result = await self.client.db["qotd"].find({"guild_id": int(guildid)}).to_list(
-                length=None
-            )
+    async def ProcesssQOTD(self, results):
+        async with self.semaphore:
+            try:
+                postdate = results.get("nextdate", None)
+                if postdate is None or postdate > datetime.datetime.utcnow():
+                    return
 
-        else:
-            result = await self.client.db['qotd'].find({}).to_list(length=None)
-
-        if not result:
-            del result
-            return
-        
-
-        for results in result:
-            await asyncio.sleep(3)
-            postdate = results.get("nextdate", None)
-            if postdate is None:
-                continue
-            if not await ModuleCheck(results.get("guild_id"), "QOTD"):
-                continue
-            if postdate <= datetime.datetime.utcnow():
-                messages = []
-                messages = results.get("messages", [])
-                try:
-                    guild = await self.client.fetch_guild(
-                        int(results.get("guild_id", None))
-                    )
-                except:
-                    guild = None
+                guild_id = int(results.get("guild_id"))
+                guild = await self.client.fetch_guild(guild_id)
                 if guild is None:
-                    continue
+                    return
 
+                messages = results.get("messages", [])
                 question = await self.fetch_question(messages, guild)
                 if question:
                     messages.append(question)
 
                 ChannelID = results.get("channel_id", None)
                 if ChannelID is None:
-                    continue
+                    return
                 ChannelID = int(ChannelID)
                 try:
                     channel = await guild.fetch_channel(ChannelID)
-                except Exception as e:
-                    channel = None
-                if channel is None:
-                    continue
+                except Exception:
+                    return
 
                 pingmsg = (
                     f"<@&{results.get('pingrole')}>" if results.get("pingrole") else ""
@@ -93,50 +72,50 @@ class qotd(commands.Cog):
                     color=discord.Color.yellow(),
                     timestamp=datetime.datetime.utcnow(),
                 )
-                if not results.get("day"):
-                    day = len(messages)
-                else:
-                    day = results.get("day") + 1
+
+                day = results.get("day", 0) + 1
                 embed.set_footer(
                     text=f"Day #{day}",
                     icon_url="https://cdn.discordapp.com/emojis/1231270156647403630.webp?size=96&quality=lossless",
                 )
-                logging.info(f"[👀] Sending QOTD for {guild.name} (ID: {guild.id})")
-                try:
-                    msg = await channel.send(
-                        content=pingmsg,
-                        embed=embed,
-                        allowed_mentions=discord.AllowedMentions(roles=True),
-                    )
-                except Exception as e:
-                    continue
-                try:
-                    await self.client.db['qotd'].update_one(
-                        {"guild_id": int(results["guild_id"])},
-                        {
-                            "$set": {
-                                "nextdate": datetime.datetime.utcnow()
-                                + datetime.timedelta(days=1),
-                                "messages": messages,
-                                "day": day,
-                            }
-                        },
-                        upsert=True,
-                    )
-                except Exception as e:
-                    logging.warn(
-                        f"{guild.name}(ID: {guild.id}) failed to update QOTD. {e}"
-                    )
-                    continue
-                if results.get("qotdthread") is True:
-                    try:
-                        await msg.create_thread(name="QOTD Discussion")
-                    except Exception as e:
-                        continue
-        
-                del result
-                del messages
-            
+
+                msg = await channel.send(
+                    content=pingmsg,
+                    embed=embed,
+                    allowed_mentions=discord.AllowedMentions(roles=True),
+                )
+
+                await self.client.db["qotd"].update_one(
+                    {"guild_id": guild_id},
+                    {
+                        "$set": {
+                            "nextdate": datetime.datetime.utcnow()
+                            + datetime.timedelta(days=1),
+                            "messages": messages,
+                            "day": day,
+                        }
+                    },
+                    upsert=True,
+                )
+
+                if results.get("qotdthread"):
+                    await msg.create_thread(name="QOTD Discussion")
+
+            except Exception as e:
+                logging.warn(f"Error processing QOTD for guild {guild_id}: {e}")
+
+    @tasks.loop(minutes=15, reconnect=True)
+    async def sendqotd(self) -> None:
+        print("[👀] Checking QOTD")
+        result = None
+        filter = {"nextdate": {"$lte": datetime.datetime.utcnow()}}
+        if bool(environment == "custom"):
+            filter["guild_id"] = int(guildid)
+        if not result:
+            return
+
+        tasks = [self.ProcesssQOTD(results) for results in result]
+        await asyncio.gather(*tasks)
 
     @commands.Cog.listener()
     async def on_ready(self):
