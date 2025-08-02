b6be9bd0db4988e7a00cc7c4e07f3e6ba8bfb6bd
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-08-02 15:24:31 +0100

    Update Infractions.py

--- Cogs/Configuration/Components/Infractions.py
+++ Cogs/Configuration/Components/Infractions.py
@@ -95,7 +95,7 @@ class InfractionOption(discord.ui.Select):
             )
         elif selection == "Webhook":
             if not await premium(interaction.guild.id):
-                return await interaction.followup.send(embed=NoPremium, view=Support())
+                return await interaction.followup.send(embed=NoPremium(), view=Support())
             
             embed = await WebhookEmbed(interaction, Config)
             view = WebButton(interaction.user)
